805184294d26bb349bff1cfc8a6e8745f7e7ce85
Peter Czimmermann <xczimi@ace.xczimi.com>
2012-09-15 19:37:16 +0000

    init

---
+++ index.php
@@ -0,0 +1,9 @@
+<html><body>Coming. <?php 
+$diffsecond = strtotime('2011-02-17 16:48 PST')-time(); 
+
+//echo floor($diffsecond/60/60);
+?>
+<p>
+<a href="http://blog.xczimi.com">Blog</a>
+</p>
+</body></html>
